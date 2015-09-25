739759a11013ab1674f5887712f803f582d27580
Daniel Kopp <dak@nerdyduck.de>
2015-09-25 15:13:29 +0200

    Created project scaffolding

---
+++ ParameterValidationUniversal/Properties/Resources.cs
@@ -0,0 +1,154 @@
+﻿#region Copyright
+/*******************************************************************************
+ * <copyright file="Resources.cs" owner="Daniel Kopp">
+ * Copyright 2015 Daniel Kopp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * </copyright>
+ * <author name="Daniel Kopp" email="dak@nerdyduck.de" />
+ * <assembly name="NerdyDuck.ParameterValidation">
+ * Validation and serialization of parameter values for .NET
+ * </assembly>
+ * <file name="Resources.cs" date="2015-09-25">
+ * Helper class to access localized string resources.
+ * </file>
+ ******************************************************************************/
+#endregion
+
+using System;
+
+namespace NerdyDuck.ParameterValidation.Properties
+{
+	/// <summary>
+	/// Helper class to access localized string resources.
+	/// </summary>
+	internal static class Resources
+	{
+		#region String resource properties
+		/// <summary>
+		/// Gets a lozalized string similar to "Temp.".
+		/// </summary>
+		internal static string Temp
+		{
+			get { return GetResource("Temp"); }
+		}
+		#endregion
+
+#if WINDOWS_UWP
+		#region Private fields
+		private static Windows.ApplicationModel.Resources.Core.ResourceMap mResourceMap;
+		private static Windows.ApplicationModel.Resources.Core.ResourceContext mContext;
+		#endregion
+
+		#region Properties
+		/// <summary>
+		/// Gets the main resource map of the assembly.
+		/// </summary>
+		internal static Windows.ApplicationModel.Resources.Core.ResourceMap ResourceMap
+		{
+			get
+			{
+				if (object.ReferenceEquals(mResourceMap, null))
+				{
+					mResourceMap = Windows.ApplicationModel.Resources.Core.ResourceManager.Current.MainResourceMap;
+				}
+
+				return mResourceMap;
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the resource context to use when retrieving resources.
+		/// </summary>
+		internal static Windows.ApplicationModel.Resources.Core.ResourceContext Context
+		{
+			get { return mContext; }
+			set { mContext = value; }
+		}
+		#endregion
+
+		#region Private methods
+		/// <summary>
+		/// Retrieves a string resource using the resource map.
+		/// </summary>
+		/// <param name="name">The name of the string resource.</param>
+		/// <returns>A localized string.</returns>
+		private static string GetResource(string name)
+		{
+			Windows.ApplicationModel.Resources.Core.ResourceContext context = Context;
+			if (context == null)
+			{
+				context = Windows.ApplicationModel.Resources.Core.ResourceContext.GetForViewIndependentUse();
+			}
+
+			Windows.ApplicationModel.Resources.Core.ResourceCandidate resourceCandidate = ResourceMap.GetValue("NerdyDuck.ParameterValidation/Resources/" + name, context);
+
+			if (resourceCandidate == null)
+			{
+				throw new ArgumentOutOfRangeException("name");
+			}
+
+			return resourceCandidate.ValueAsString;
+		}
+		#endregion
+#endif
+
+#if WINDOWS_DESKTOP
+		#region Private fields
+		private static System.Resources.ResourceManager mResourceManager;
+		private static System.Globalization.CultureInfo mResourceCulture;
+		#endregion
+
+		#region Properties
+		/// <summary>
+		/// Returns the cached ResourceManager instance used by this class.
+		/// </summary>
+		[System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Advanced)]
+		internal static System.Resources.ResourceManager ResourceManager
+		{
+			get
+			{
+				if (object.ReferenceEquals(mResourceManager, null))
+				{
+					System.Resources.ResourceManager temp = new System.Resources.ResourceManager("NerdyDuck.ParameterValidation.Properties.Resources", typeof(Resources).Assembly);
+					mResourceManager = temp;
+				}
+				return mResourceManager;
+			}
+		}
+
+		/// <summary>
+		/// Overrides the current thread's CurrentUICulture property for all resource lookups using this strongly typed resource class.
+		/// </summary>
+		[System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Advanced)]
+		internal static System.Globalization.CultureInfo Culture
+		{
+			get { return mResourceCulture; }
+			set { mResourceCulture = value; }
+		}
+		#endregion
+
+		#region Private methods
+		/// <summary>
+		/// Retrieves a string resource using the resource manager.
+		/// </summary>
+		/// <param name="name">The name of the string resource.</param>
+		/// <returns>A localized string.</returns>
+		private static string GetResource(string name)
+		{
+			return ResourceManager.GetString(name, mResourceCulture);
+		}
+		#endregion
+#endif
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
+## --- Soon to come ---
 # NerdyDuck.ParameterValidation
-Validation and serialization of arbitrary (parameter) values stored as strings
+
+This project provides a set of constraint classes that can be used to validate arbitrary values, like application settings. It also contains classes to consistently serialize and deserialize various data types into strings that can then be stored in formats like XML or databases.
+
+#### Platforms
+- .NET Framework 4.5 or newer for desktop applications
+- Universal App Platform (UAP) or Universal Windows Platform (UWP) 10.0 (Windows 10) or newer for Windows Store Apps and [Windows 10 IoT](https://dev.windows.com/en-us/iot)
+
+**--- More information in preparation ---**
